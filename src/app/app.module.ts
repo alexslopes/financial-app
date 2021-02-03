9e7bcb3477c65c09f7cf366ce85885f80423790c
Alex <alexstlopes@gmail.com>
2021-02-02 23:52:59 -0300

    Iniciado Forms

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,13 +11,13 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { MatIconModule } from '@angular/material/icon';
 import { NewRegistryComponent } from './new-registry/new-registry.component';
 import { MatSidenavModule } from '@angular/material/sidenav';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MainNavComponent } from './main-nav/main-nav.component';
 import { LayoutModule } from '@angular/cdk/layout';
 import { MatButtonModule } from '@angular/material/button';
 import { MatListModule } from '@angular/material/list';
-import {MatGridListModule} from '@angular/material/grid-list';
-import {MatTableModule} from '@angular/material/table';
+import { MatGridListModule} from '@angular/material/grid-list';
+import { MatTableModule} from '@angular/material/table';
 
 import { NgxChartsModule } from '@swimlane/ngx-charts';
 import { OutgoingComponent } from './outgoing/outgoing.component';
@@ -42,6 +42,7 @@ import { AppRoutingModule } from './app-routing/app-routing.module';
     BrowserAnimationsModule,
     MatSidenavModule,
     FormsModule,
+    ReactiveFormsModule,
     LayoutModule,
     MatButtonModule,
     MatListModule,
--- src/app/new-registry/new-registry.component.ts
+++ src/app/new-registry/new-registry.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-new-registry',
@@ -7,9 +8,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewRegistryComponent implements OnInit {
 
-  constructor() { }
+  registryForm: FormGroup;
+
+  constructor(
+    private rf: FormBuilder
+  ) {
+    this.createForm();
+   }
 
   ngOnInit(): void {
+  }
+
+  createForm() {
+    this.registryForm = this.rf.group({
+      description: ['', Validators.required],
+      value: ['', Validators.required],
+      category: ['', ],
+      date: ['', ],
+      type: ['', Validators.required],
+    });
   }
 
 }
