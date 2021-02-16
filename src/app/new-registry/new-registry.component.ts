d0538cc597dde540a16a3b6c0ddda716e0719d8a
Alex <alexstlopes@gmail.com>
2021-02-16 13:00:45 -0300

    Acrescentado validação, problema na msg de erro

--- src/app/new-registry/new-registry.component.ts
+++ src/app/new-registry/new-registry.component.ts
@@ -10,6 +10,26 @@ export class NewRegistryComponent implements OnInit {
 
   registryForm: FormGroup;
 
+  formErrors = {
+    'description': '',
+    'value': '',
+    'category': '',
+    'date': ''
+  }
+
+  validationMessages = {
+    'description': {
+      'required': 'Description is required.'
+    },
+    'value': {
+      'required': 'Value is required.',
+      'pattern': 'Value must contain only numbers.'
+    },
+    'date': {
+      'required': 'Date is required.'
+    }
+  };
+
   constructor(
     private rf: FormBuilder
   ) {
@@ -23,10 +43,30 @@ export class NewRegistryComponent implements OnInit {
     this.registryForm = this.rf.group({
       description: ['', Validators.required],
       value: ['', Validators.required],
-      category: ['', ],
-      date: ['', ],
-      type: ['', Validators.required],
+      date: ['', Validators.required]
     });
+
+    this.registryForm.valueChanges
+      .subscribe(data => this.onValueChanged(data));
+  }
+
+  onValueChanged(data?: any) {
+    if (!this.registryForm) { return; }
+    const form = this.registryForm;
+    for (const field in this.formErrors) {
+      if (this.formErrors.hasOwnProperty(field)) {
+        this.formErrors[field] = '';
+        const control = form.get(field);
+        if (control && control.dirty && !control.valid) {
+          const messages = this.validationMessages[field];
+          for (const key in control.errors) {
+            if (control.errors.hasOwnProperty(key)) {
+              this.formErrors[field] += messages[key] + ' ';
+            }
+          }
+        }
+      }
+    }
   }
 
   onSubmit() {
